c2f265098f9ffff1f2b0e794d4020351452104b6
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-02-12 01:14:54 -0800

    Removed input box background color

--- components/todo.js
+++ components/todo.js
@@ -5,6 +5,9 @@ $('#todo-add').on('click', addToDo);
 
 function addToDo() {
   let todo = $('#todo').val();
+  if (!todo) {
+     return null;
+  }
   $('#todo-ul').append('<li>' + todo +'<span><button class="ml-2 mb-2 bg-danger text-white" id="delete-todo">Delete</button></span></li>');
-
+  $('#todo').val('');
 }
